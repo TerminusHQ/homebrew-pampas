8bea7d9ac350d0dffa2a4a12ec6a7e952a6a5e03
gengrong <gr@terminus.io>
2017-05-17 14:53:14 +0800

    update version

--- pampas.rb
+++ pampas.rb
@@ -1,18 +1,18 @@
 class Pampas < Formula
   desc "pampas command line interface"
   homepage "https://www.terminus.io/"
-  url "http://terminus-paas.oss.aliyuncs.com/dist/pampas/pampas.0.2.1.tar.gz"
-  version "0.2.1"
-  sha256 "cc142a4229db000c78c0489ec367605c2aa92129f9f5f296f37b248ffa0f7f23"
+  url "http://terminus-paas.oss.aliyuncs.com/dist/pampas/pampas.0.2.2.tar.gz"
+  version "0.2.2"
+  sha256 "c682dc5664ab2dc973494137778e18d0806d909a93e396e38447230c28d8a138"
 
   depends_on "maven"
   depends_on "git"
-  depends_on "node"
- # depends_on "docker" => :recommended
+# depends_on "node"
+# depends_on "docker" => :recommended
 
   def install
     bin.install Dir["bin/*"]
-    lib.install Dir["lib/*"]
+    # lib.install Dir["lib/*"]
     prefix.install "install-mac.sh"
   end
 
