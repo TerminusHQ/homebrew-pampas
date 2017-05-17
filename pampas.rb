2782c8af2ee6cb19a5932ba9acbf1ee8643f836f
gengrong <gr@terminus.io>
2017-05-17 15:04:41 +0800

    update pampas version

--- pampas.rb
+++ pampas.rb
@@ -1,9 +1,9 @@
 class Pampas < Formula
   desc "pampas command line interface"
   homepage "https://www.terminus.io/"
-  url "http://terminus-paas.oss.aliyuncs.com/dist/pampas/pampas.0.2.2.tar.gz"
-  version "0.2.2"
-  sha256 "c682dc5664ab2dc973494137778e18d0806d909a93e396e38447230c28d8a138"
+  url "http://terminus-paas.oss.aliyuncs.com/dist/pampas/pampas.0.2.3.tar.gz"
+  version "0.2.3"
+  sha256 "ba426dd69a62bbae9b2742a20fed7204d729a163b3e2d43686cb9c62a72a049b"
 
   depends_on "maven"
   depends_on "git"
